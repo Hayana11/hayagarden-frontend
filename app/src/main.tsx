8292123d644d311ebd008bd31a59a44e3362683a
Fyodor <fyodor@love-style.xyz>
2026-07-10 13:11:02 +0000

    fix(app): preserve WebView dash and Pocket repair fixes

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+window.__dashProbe?.('module loaded')
+
+window.__dashProbe?.('render called')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
