b0e30b551372c4a854e0fd5791645601abc13df6
Hayana11 <ejohogug80@gmail.com>
2026-07-11 15:47:59 +0800

    fix: type dashboard probe hook

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+declare global {
+  interface Window {
+    __dashProbe?: (message: string) => void
+  }
+}
+
 window.__dashProbe?.('module loaded')
 
 window.__dashProbe?.('render called')
