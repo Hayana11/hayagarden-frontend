8fb4a23463061031d8ef024e622aa5672142cb50
Hayana11 <ejohogug80@gmail.com>
2026-08-20 10:53:11 +0800

    test: include diary provider in fixed surface assertion

--- tests/test_execution_fence.py
+++ tests/test_execution_fence.py
@@ -303,8 +303,9 @@ class ExecutionFenceTests(unittest.TestCase):
             set(physical_surface_names()),
             {
                 "Read", "Glob", "Grep", "WebSearch", "WebFetch",
-                "mcp__home__search_memories", "mcp__home__get_light_status",
-                "mcp__home__get_todos", "mcp__home__add_todo",
+                "mcp__home__search_memories", "mcp__home__write_diary",
+                "mcp__home__get_light_status", "mcp__home__get_todos",
+                "mcp__home__add_todo",
                 "mcp__home__get_countdowns", "mcp__home__get_ledger",
                 "mcp__home__get_ledger_budget", "mcp__home__add_ledger",
             },
